475c94d0b0422ef0b7aae48cb173429c0a4279e8
Gene Hackerman <arias.rafael.1984@gmail.com>
2022-03-11 10:54:32 -0500

    fixing spelling errors to make viewByManager run without err

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ function viewByDepartment() {
 }
 
 function viewByManager() {
-    const query = `SELECT CONCAT(manager.fist_name, '', manager.last_name) AS manager, department.name AS department, employee.id, employee.first_name, employee.last_name, role.title
+    const query = `SELECT CONCAT(manager.first_name, '', manager.last_name) AS manager, department.name AS department, employee.id, employee.first_name, employee.last_name, role.title
     FROM employee
     LEFT JOIN employee manager ON manager.id = employee.manager_id
     INNER JOIN role ON (role.id = employee.role_id && employee.manager_id != 'NULL')
